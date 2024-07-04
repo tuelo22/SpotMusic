7599c85c5ac1c822cbe5756337f404997faaa892
Julio Francis Pereira dos Santos Mendonça <julio_fpsm@hotmail.com>
2024-07-04 01:41:18 -0300

    Criando cadastro de musica.

---
+++ SpotMusic.Admin/Controllers/MusicaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpotMusic.Admin.Controllers
+{
+    public class MusicaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
